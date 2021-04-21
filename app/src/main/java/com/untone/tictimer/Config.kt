22a5b67fbc862ace2657083d9422fff688b88fbe
Matthew <43993777+matthew-5pl@users.noreply.github.com>
2021-04-21 21:59:15 +0200

    start work on cool stuff

---
+++ app/src/main/java/com/untone/tictimer/Config.kt
@@ -0,0 +1,6 @@
+package com.untone.tictimer
+
+class Config {
+    private val key: String = "DVxhYXrK8nFJrJxjju4D6E4eKTSHKTDcpKnPUYYwZP2HEWrKPKA8PDkZFVsRzRWV"
+    private val type: String = "dev"
+}
